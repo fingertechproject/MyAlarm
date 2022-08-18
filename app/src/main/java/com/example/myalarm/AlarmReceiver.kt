a612acec98771012f99e95d901f9df7eaa1f3b8c
Khalid <fingertechproject@gmail.com>
2022-08-19 01:03:55 +0530

    alarm added

---
+++ app/src/main/java/com/example/myalarm/AlarmReceiver.kt
@@ -0,0 +1,29 @@
+package com.example.myalarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.os.Vibrator
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+
+
+class AlarmReceiver : BroadcastReceiver() {
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    override fun onReceive(context: Context, intent: Intent) {
+
+        Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show()
+        var alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        }
+
+        // setting default ringtone
+        val ringtone = RingtoneManager.getRingtone(context, alarmUri)
+
+        // play ringtone
+        ringtone.play()
+    }
+}
